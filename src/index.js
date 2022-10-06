6ef0228f397fd4c33d0476b373d663fdae4e9746
sy <sykoivisto@gmail.com>
2022-10-06 14:28:30 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 //import _ from 'lodash';
-import myName from './myName'
+import myName from './myName';
+import './style.css';
+import pic from './640px-Felis_catus-cat_on_snow.jpg';
 
 function component() {
     const element = document.createElement('div');
 
     //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.textContent = myName('Silas')
+    element.classList.add('hello');
+
+    const myPic = new Image();
+    myPic.src = pic;
+
+    element.appendChild(myPic);
 
     return element;
 }
